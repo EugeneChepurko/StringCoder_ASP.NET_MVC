9a3eeda8098776498df330dea6c211d55f8ca9eb
Eugene <chepurko0@gmail.com>
2019-08-12 19:56:42 +0300

    last commit? Noo

--- StringCoder_ASP.NET_MVC/Controllers/HomeController.cs
+++ StringCoder_ASP.NET_MVC/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace StringCoder_ASP.NET_MVC.Controllers
 {
@@ -14,24 +15,14 @@ namespace StringCoder_ASP.NET_MVC.Controllers
         readonly string[] charsToRemove = new string[] { ",", ".", ";", ":", "@", "!", "#", "$", "%", "*", "(", ")", "[", "]", "^", "?", "|", "+", "-", "&", @"\", "/", "'", "\"" };
         private static int key = 0;
         int Xkey, Ytext;
-        string current = null;
-        string prev = null;
-        public string tempEncode = null;
 
-        private static List<string> ciphers = new List<string>() { "Caesar cipher", "Vigenere cipher", "HashFunc" };
+        private static List<string> ciphers = new List<string>() { "Caesar cipher", "Vigenere cipher" };
 
-        //IEnumerable<SelectListItem> ciphers1 = new List<SelectListItem>()
-        //{
-        //    new SelectListItem() { Text="Caesar cipher", Value="CC"},
-        //    new SelectListItem() { Text="Vigenere cipher", Value="VC"},
-        //    new SelectListItem() { Text="HashFunc", Value="HFC"}
-        //};
-    
         [HttpPost]
         public ActionResult EncodeText(FormData formData, FormCollection form)
         {
             SelectList items = new SelectList(ciphers);
-            ViewBag.Ciphers = items;
+
             // Caesar cipher encode
             if (Request.Form["Ciphers"] == "Caesar cipher" && formData.tbkey != "")
             {
@@ -60,8 +51,6 @@ namespace StringCoder_ASP.NET_MVC.Controllers
                                 char.ToLower(text[i]);
 
                                 formData.EncodedText += Convert.ToChar(alphabet[(j + key) % alphabet.Length]);
-                                tempEncode = formData.EncodedText;
-                                ViewBag.EncText = tempEncode;
                                 break;
                             }
                         }
@@ -72,6 +61,96 @@ namespace StringCoder_ASP.NET_MVC.Controllers
                     ViewBag.Exception = ex.Message;
                 }
             }
+
+            //  Vigenere cipher encode
+            if (Request.Form["Ciphers"] == "Vigenere cipher")
+            {
+                //Request.Form["Your decoded text:"] = "  Your decoded text:";
+                //Request.Form["Decode"] = "Decode";
+                if (formData.EncodedText != "")
+                {
+                    formData.EncodedText = "";
+                }
+                foreach (var item in charsToRemove)
+                {
+                    formData.YourText = formData.YourText.Replace(item, string.Empty);
+                }
+                char[] text = formData.YourText.ToCharArray().Where(s => !char.IsWhiteSpace(s)).ToArray();
+                char[] key = formData.tbkey.ToCharArray();
+                try
+                {
+                    char[,] Vigenere_Table = new char[26, 26];
+
+                    int temp = 0;
+                    for (int i = 0; i < alphabet.Length; i++)
+                    {
+                        for (int j = 0; j < 26; j++)
+                        {
+                            temp = j + i;
+                            if (temp >= 26)
+                            {
+                                temp = temp % 26;
+                            }
+                            Vigenere_Table[i, j] = alphabet[temp];
+                        }
+                    }
+
+                    for (int t = 0, k = 0; t < text.Length || k < key.Length; t++, k++)
+                    {
+                        if (t >= text.Length)
+                        {
+                            break;
+                        }
+                        if (k == key.Length/*t % key.Length == 0*/)
+                        {
+                            k = 0;
+                            for (int y = 0; y <= alphabet.Length; y++)
+                            {
+                                if (text[t].ToString() == alphabet[y].ToString())
+                                {
+                                    Ytext = y;
+                                    for (int x = 0; x <= alphabet.Length; x++)
+                                    {
+                                        if (key[k].ToString() == alphabet[x].ToString())
+                                        {
+                                            Xkey = x;
+                                            formData.EncodedText += Vigenere_Table[Ytext, Xkey].ToString();
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int y = 0; y <= alphabet.Length; y++)
+                            {
+                                if (text[t].ToString() == alphabet[y].ToString())
+                                {
+                                    Ytext = y;
+                                    for (int x = 0; x <= alphabet.Length; x++)
+                                    {
+                                        if (key[k].ToString() == alphabet[x].ToString())
+                                        {
+                                            Xkey = x;
+                                            formData.EncodedText += Vigenere_Table[Ytext, Xkey].ToString();
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Exception = ex.Message;
+                }
+            }        
+
+            ViewBag.Ciphers = items;
             return View(formData);
         }
 
@@ -116,6 +195,88 @@ namespace StringCoder_ASP.NET_MVC.Controllers
                     ViewBag.Exception = ex.Message;
                 }
             }
+
+            //  Vigenere cipher decode
+            if (Request.Form["Ciphers"] == "Vigenere cipher")
+            {
+                if (formData.DecodedText != "")
+                {
+                    formData.DecodedText = "";
+                }
+                char[] text = formData.EncodedText.ToCharArray().Where(s => !char.IsWhiteSpace(s)).ToArray();
+                char[] key = formData.tbkey.ToCharArray();
+                try
+                {
+                    char[,] Vigenere_Table = new char[26, 26];
+
+                    int temp = 0;
+                    for (int i = 0; i < alphabet.Length; i++)
+                    {
+                        for (int j = 0; j < 26; j++)
+                        {
+                            temp = j + i;
+                            if (temp >= 26)
+                            {
+                                temp = temp % 26;
+                            }
+                            Vigenere_Table[i, j] = alphabet[temp];
+                        }
+                    }
+                    // DECRYPT VIGENERE
+                    for (int t = 0, k = 0; t < text.Length || k < key.Length; t++, k++)
+                    {
+                        if (t >= text.Length)
+                        {
+                            break;
+                        }
+                        if (k == key.Length)
+                        {
+                            k = 0;
+                            for (int y = 0; y <= alphabet.Length; y++)
+                            {
+                                if (key[k].ToString() == alphabet[y].ToString())
+                                {
+                                    Xkey = y;
+                                    for (int x = 0; x <= alphabet.Length; x++)
+                                    {
+                                        Ytext = x;
+                                        if (Vigenere_Table[Ytext, Xkey].ToString() == text[t].ToString())
+                                        {
+                                            formData.DecodedText += alphabet[x].ToString();
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int y = 0; y <= alphabet.Length; y++)
+                            {
+                                if (key[k].ToString() == alphabet[y].ToString())
+                                {
+                                    Xkey = y;
+                                    for (int x = 0; x <= alphabet.Length; x++)
+                                    {
+                                        Ytext = x;
+                                        if (Vigenere_Table[Ytext, Xkey].ToString() == text[t].ToString())
+                                        {
+                                            formData.DecodedText += alphabet[x].ToString();
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Exception = ex.Message;
+                }
+            }         
             return View(formData);
         }
         [HttpGet]
@@ -128,15 +289,11 @@ namespace StringCoder_ASP.NET_MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
--- StringCoder_ASP.NET_MVC/Models/FormData.cs
+++ StringCoder_ASP.NET_MVC/Models/FormData.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.UI.WebControls;
 
 namespace StringCoder_ASP.NET_MVC.Models
 {
@@ -11,5 +8,9 @@ namespace StringCoder_ASP.NET_MVC.Models
         public string EncodedText { get; set; }
         public string DecodedText { get; set; }
         public string tbkey { get; set; }
+        public Label lbDecText { get; set; }
+        //public string tbkey { get; set; }
+        public Button Decode { get; set; }
+        public DropDownList Ciphers { get; set; }
     }
 }
